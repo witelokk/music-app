f04065e82d29bae2a2dc01afffb8fe69be67f5ac
witelokk <witelokk@gmail.com>
2026-03-24 18:33:09 +0300

    fix theme setting

--- composeApp/src/androidMain/kotlin/com/witelokk/musicapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/witelokk/musicapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.witelokk.musicapp
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
@@ -9,7 +11,11 @@ import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(
+                Color.TRANSPARENT, Color.TRANSPARENT
+            )
+        )
         super.onCreate(savedInstanceState)
 
         setContent {
--- composeApp/src/commonMain/kotlin/com/witelokk/musicapp/viewmodel/ThemeViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/witelokk/musicapp/viewmodel/ThemeViewModel.kt
@@ -4,28 +4,23 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.witelokk.musicapp.SettingsRepository
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class ThemeViewModel(private val settingsRepository: SettingsRepository) : ViewModel() {
-//    val theme: StateFlow<String> = settingsRepository.theme.stateIn(
-//        viewModelScope,
-//        started = SharingStarted.WhileSubscribed(5000),
-//        initialValue = ""
-//    )
-    val _theme = MutableStateFlow("system")
+    private val _theme = MutableStateFlow("system")
     val theme = _theme.asStateFlow()
 
     init {
-//        coro
+        runBlocking { _theme.value = settingsRepository.theme.first() }
     }
 
     fun setTheme(theme: String) {
-//        viewModelScope.launch {
-//            settingsRepository.setTheme(theme)
-//        }
+        viewModelScope.launch {
+            settingsRepository.setTheme(theme)
+        }
+        _theme.value = theme
     }
 }
