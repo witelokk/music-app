345ccf712aed806a803e57151544bd70e9991bdb
witelokk <witelokk@gmail.com>
2026-04-16 20:43:09 +0300

    rename platform specific files

--- composeApp/src/iosMain/kotlin/com/witelokk/musicapp/PlatformModule.ios.kt
+++ composeApp/src/iosMain/kotlin/com/witelokk/musicapp/PlatformModule.ios.kt
@@ -1,18 +1,13 @@
 package com.witelokk.musicapp
 
-import com.witelokk.musicapp.api.models.Song
-import com.witelokk.musicapp.data.PlayerState
 import io.ktor.client.engine.HttpClientEngineFactory
 import io.ktor.client.engine.darwin.Darwin
 import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.coroutines.flow.MutableStateFlow
 import org.koin.dsl.module
 import platform.Foundation.NSDocumentDirectory
 import platform.Foundation.NSFileManager
 import platform.Foundation.NSURL
 import platform.Foundation.NSUserDomainMask
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
 
 @OptIn(ExperimentalForeignApi::class)
 actual val platformModule = module {
